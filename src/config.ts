ffcdace71a0ebe56b3582cc86a52f0b925260392
paulhmorris <paulh.morris@gmail.com>
2023-08-16 16:58:44 -0500

    feat: first two sections, tw config

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,2 @@
 export const SITE_TITLE = "Friendly Bear Labs Starter" as const;
+export const COMPANY_NAME = "Friendly Bear Labs" as const;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,13 +3,25 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontWeight: {
+      light: 300,
+      normal: 400,
+      medium: 500,
+      bold: 700,
+      black: 900,
+    },
     extend: {
       fontFamily: {
-        sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
+        sans: ["Work Sans", "Inter", ...defaultTheme.fontFamily.sans],
       },
       colors: {
         primary: "hsl(var(--primary) / <alpha-value>)",
         secondary: "hsl(var(--secondary) / <alpha-value>)",
+        accent: {
+          DEFAULT: "hsl(var(--accent) / <alpha-value>)",
+          dark: "hsl(var(--accent-dark) / <alpha-value>)",
+          light: "hsl(var(--accent-light) / <alpha-value>)",
+        },
       },
     },
   },
