b26a6b7fe83598e70cfebc6953ce221ec2148f82
paulhmorris <paulh.morris@gmail.com>
2023-08-23 08:32:00 -0500

    refactor: styles and misc

--- src/lib/config.ts
+++
@@ -1,2 +0,0 @@
-export const SITE_NAME = "Friendly Bear Labs Starter";
-export const COMPANY_TITLE = "Friendly Bear Labs";
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,13 +3,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    fontWeight: {
-      light: 300,
-      normal: 400,
-      medium: 500,
-      bold: 700,
-      black: 900,
-    },
     extend: {
       spacing: {
         4.5: "1.125rem",
@@ -18,17 +11,28 @@ module.exports = {
         sans: ["Work Sans Variable", ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        bg: "hsl(var(--bg) / <alpha-value>)",
-        primary: "hsl(var(--primary) / <alpha-value>)",
-        secondary: "hsl(var(--secondary) / <alpha-value>)",
+        primary: {
+          DEFAULT: "hsl(var(--primary) / <alpha-value>)",
+          foreground: "hsl(var(--primary-foreground) / <alpha-value>)",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
+          foreground: "hsl(var(--secondary-foreground) / <alpha-value>)",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted) / <alpha-value>)",
+          foreground: "hsl(var(--muted-foreground) / <alpha-value>)",
+        },
         accent: {
           DEFAULT: "hsl(var(--accent) / <alpha-value>)",
+          foreground: "hsl(var(--accent-foreground) / <alpha-value>)",
           dark: "hsl(var(--accent-dark) / <alpha-value>)",
           light: "hsl(var(--accent-light) / <alpha-value>)",
         },
-        "muted-bg": "hsl(var(--muted-bg) / <alpha-value>)",
-        "muted-foreground": "hsl(var(--muted-foreground) / <alpha-value>)",
-        destructive: "hsl(var(--destructive) / <alpha-value>)",
+        destructive: {
+          DEFAULT: "var(--destructive)",
+          foreground: "var(--destructive-foreground)",
+        },
       },
     },
   },
