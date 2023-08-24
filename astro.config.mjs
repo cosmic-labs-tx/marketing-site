fcc5cc8bd887e7a21cfae8ccdad2b427fa5b67cb
paulhmorris <paulh.morris@gmail.com>
2023-08-24 16:23:18 -0500

    feat: partytown, turnstile fix, navlinks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,24 @@
+import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
-import prefetch from "@astrojs/prefetch";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://friendlybearlabs.com",
   output: "static",
-  integrations: [tailwind({ applyBaseStyles: false }), sitemap(), prefetch(), icon({ include: { tabler: ["*"] } })],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), sitemap(), prefetch(), icon({
+    include: {
+      tabler: ["*"]
+    }
+  }), partytown()],
   experimental: {
     assets: true,
-    viewTransitions: true,
-  },
+    viewTransitions: true
+  }
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client-image" />
+
+interface DocumentEventMap {
+  "astro:load";
+  "astro:beforeload";
+}
