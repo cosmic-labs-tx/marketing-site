dc629b200b6ad03f7651bfaee85e6db15203bd93
paulhmorris <paulh.morris@gmail.com>
2023-08-24 16:52:44 -0500

    feat: add env to interface, troubleshoot envs

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,20 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client-image" />
 
+interface Window {
+  _turnstileCb: () => void;
+}
+
 interface DocumentEventMap {
   "astro:load";
   "astro:beforeload";
+}
+
+interface ImportMetaEnv {
+  readonly PUBLIC_TURNSTILE_SITEKEY: string;
+  readonly PUBLIC_UMAMI_WEBSITE_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
