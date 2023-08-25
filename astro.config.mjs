53ce4fb83512826716e4ec25f222e184410c29fc
paulhmorris <paulh.morris@gmail.com>
2023-08-25 12:51:14 -0500

    fix: nav event listeners

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,22 @@
+import partytown from "@astrojs/partytown";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
-import partytown from "@astrojs/partytown";
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://friendlybearlabs.com",
   output: "static",
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), sitemap(), prefetch(), icon({
-    include: {
-      tabler: ["*"]
-    }
-  }), partytown()],
+  integrations: [
+    partytown(),
+    prefetch(),
+    tailwind({ applyBaseStyles: false }),
+    icon({ include: { tabler: ["*"] } }),
+    process.env.SITEMAP !== "false" && sitemap(),
+  ],
   experimental: {
     assets: true,
-    viewTransitions: true
-  }
+    viewTransitions: true,
+  },
 });
