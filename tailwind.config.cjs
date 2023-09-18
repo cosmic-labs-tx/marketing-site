26f44da547c0314ba02a2ddf79f4f29a42ef6595
paulhmorris <paulh.morris@gmail.com>
2023-09-18 16:30:46 -0500

    feat: new pricing, misc clean up

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,36 +3,38 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      white: "#fff",
+      primary: {
+        DEFAULT: "hsl(var(--primary) / <alpha-value>)",
+        foreground: "hsl(var(--primary-foreground) / <alpha-value>)",
+      },
+      secondary: {
+        DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
+        foreground: "hsl(var(--secondary-foreground) / <alpha-value>)",
+      },
+      muted: {
+        DEFAULT: "hsl(var(--muted) / <alpha-value>)",
+        foreground: "hsl(var(--muted-foreground) / <alpha-value>)",
+      },
+      accent: {
+        DEFAULT: "hsl(var(--accent) / <alpha-value>)",
+        foreground: "hsl(var(--accent-foreground) / <alpha-value>)",
+        dark: "hsl(var(--accent-dark) / <alpha-value>)",
+        light: "hsl(var(--accent-light) / <alpha-value>)",
+      },
+      destructive: {
+        DEFAULT: "var(--destructive)",
+        foreground: "var(--destructive-foreground)",
+      },
+    },
     extend: {
       spacing: {
         4.5: "1.125rem",
       },
       fontFamily: {
         sans: ["Work Sans Variable", ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        primary: {
-          DEFAULT: "hsl(var(--primary) / <alpha-value>)",
-          foreground: "hsl(var(--primary-foreground) / <alpha-value>)",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
-          foreground: "hsl(var(--secondary-foreground) / <alpha-value>)",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted) / <alpha-value>)",
-          foreground: "hsl(var(--muted-foreground) / <alpha-value>)",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent) / <alpha-value>)",
-          foreground: "hsl(var(--accent-foreground) / <alpha-value>)",
-          dark: "hsl(var(--accent-dark) / <alpha-value>)",
-          light: "hsl(var(--accent-light) / <alpha-value>)",
-        },
-        destructive: {
-          DEFAULT: "var(--destructive)",
-          foreground: "var(--destructive-foreground)",
-        },
       },
     },
   },
